d4f6ff1560d47033e9bce62b56bc31cb635d9103
Artem Holovko <104083054+MrWhoopee@users.noreply.github.com>
2025-10-10 19:14:19 +0300

    Update success.js

--- src/js/success.js
+++ src/js/success.js
@@ -1,11 +1,14 @@
 const registerEl = document.querySelector(".register-labels-wrapper .big-blue-btn")
 const closeBtnEl = document.querySelector(".succes-close-btn")
+const closeBlueBtnEl = document.querySelector(".modal-success .big-blue-btn")
 const modalEl = document.querySelector(".backdrop-success")
 const bodyEl = document.body;
 
 
 registerEl.addEventListener('click', toggleModal) 
 closeBtnEl.addEventListener('click', toggleModal)
+closeBlueBtnEl.addEventListener('click', toggleModal)
+
 
 function toggleModal() {
     modalEl.classList.toggle("is-open-now")
